e61bc9cfa9300016656f69b972e3e0c3bc2d2fed
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-10-21 19:04:13 +0700

    slicing tokopedia

---
+++ assets/js/js.js
@@ -0,0 +1,14 @@
+function on() {
+  var menunBar = document.getElementById("menuBar");
+
+  var overlay = document.getElementById("overlay");
+
+  menunBar.classList.toggle("menuBarStyle");
+  overlay.classList.toggle("overlayStyle");
+}
+
+function kategoryOn() {
+  var kategoryy = document.getElementById("kategory");
+
+  kategoryy.classList.toggle("kategoryStyle");
+}
